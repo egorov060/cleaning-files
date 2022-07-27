5ea8ecacb2ddfdbf1a9560bf28fbe3e6a64847c6
Egorov <egor35@yandex.ru>
2022-07-27 20:35:57 +0200

    new1

--- clear-date-15day.sh
+++ clear-date-15day.sh
@@ -4,3 +4,4 @@ find /home/ftpuser/ftp/files -type f -mtime +15 -delete
 # -type f - ищем только файлы, иначе удалит и директории
 # -mtime +15 - когда был создан, +15 значит, что 15 дней назад
 # -delete - удалить найденные файлы
+#Hello
